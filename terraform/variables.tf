4d77ecbf39129d3ae05921ad62a0ea1e52534f9f
akumar5 <akshay.kumar@elliemae.com>
2018-06-22 13:47:09 -0700

    working version with my changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,18 @@
 variable control_cidr {
   description = "CIDR for maintenance: inbound traffic will be allowed from this IPs"
+  #default = "209.220.148.28/32" non-home
+  default = "67.180.52.27/32"
 }
 
 variable default_keypair_public_key {
   description = "Public Key of the default keypair"
+  #default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJRUrQqfbbJJj7TRajI+3L24Um7xlHMjh7wmxg0jQq4S5OyjkbMftPNYdMCPvHSqDVI9PRVhgMuAy6ctbRL3A3SGIwA6XlevOo3ps5q3Jl7DauyQlv7ABrCHj/eBFKbIJeHXuzj9gMGPsbkgv7GHPDCC5CY0rVlik2RqwoHTir/l46x4EYpNZUBqJ2iHzv/MCDnBZpXd3IlWbnzecOR5LKsNNWtwgEbjJnO64urwANA+AXSH/Ei4nkf2QC7j5vLkrS0bEGv0DPclWZWT9VP5Q8axUP/FJHpjMdyeyQ4IISHRc6gWE7a18CMMntqCAXMVn6ZXT9acUkSRWD0DR4BnId akumar5@OSXAKUMA5MBP15.local"
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDYSYumRQDQ3mH8IsH6G8UUxQdAoZ7tqfGsoNIKnz263sGHMrJ0cX8ohhx5QzlDSN+PuH5QUhRZuWsoMNGCdDkZCQISKX9PgFOXpQLmc8voVt/GfVoCUzB/Oc0BmOSBy5G7tXLVXIO+XhGu2/9BuAITI1+rZiv2vkBLQJTH/Do1BKYMyu3lBfKTfnfjwlYYVZrsga/OU67YX1mJ33l402oWHG9+0GByTo5JAWp2WjLHyXiMwiSDnXr7xLCigw85iPxmPntGH5Mr/3CVsfnTC3TKeZkzOGKOEuFEBfBQkWIdEKPEd1fmOQ+rQf4kyGA6UTMZIDB6ImFCocrn7ewKvgBZWABnyQnIFP7/0pHm9OcWyG8X7+gCcwhEVAP6yfIdIn9xPCblqjTXBkJdOnd2VXWtqjX73XaNRck8girRlCffOhzocRhzK50+K3nxS28fcPqlyim5FXYlnOellay1GMfecXCVLc95Jumwu4X5l82yOfb8d5tZ3pUoPZB7yxlMN7nK/jzHrPm5u2CVgn8WO9VfJn30cJ1NNilsQSKbuu4eEJtybChCb6u2WBQvD5UXPzVwNGawaF2Q7SChikQ8WE6JbiK1jPmZpVxDBweXhXK/NhOYjcLIbaJRcykof8vjhvkBwPdYDSS/9oIuqnfM1PgS8tucT73tICUOMvU0fWIKkw== akumar5@osxakuma5mbp15.corp.elmae"
 }
 
 variable default_keypair_name {
   description = "Name of the KeyPair used for all nodes"
-  default = "k8s-not-the-hardest-way"
+  default = "emk8"
 }
 
 
@@ -33,11 +37,11 @@ variable ansibleFilter {
 
 # Networking setup
 variable region {
-  default = "eu-west-1"
+  default = "us-west-2"
 }
 
 variable zone {
-  default = "eu-west-1a"
+  default = "us-west-2a"
 }
 
 ### VARIABLES BELOW MUST NOT BE CHANGED ###
@@ -63,8 +67,10 @@ variable amis {
     eu-west-1 = "ami-0d77397e"
     sa-east-1 = "ami-e93da085"
     us-east-1 = "ami-40d28157"
-    us-west-1 = "ami-6e165d0e"
-    us-west-2 = "ami-a9d276c9"
+    #us-west-1 = "ami-6e165d0e"
+    us-west-1 = "ami-8d948ced"
+    #us-west-2 = "ami-a9d276c9"
+    us-west-2 = "ami-db710fa3"
   }
 }
 variable default_instance_user {
